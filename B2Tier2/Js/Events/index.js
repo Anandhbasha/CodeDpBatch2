fefd6024f83d363468d6bf28abf37459f6079039
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-16 18:56:28 +0530

    events

---
+++ B2Tier2/Js/Events/index.js
@@ -0,0 +1,35 @@
+// let button = document.getElementById("btn")
+
+
+// let button = document.getElementsByClassName("btn")[0]
+// let button = document.getElementsByTagName("button")[0]
+// let button = document.querySelectorAll(".btn")
+// const show = ()=>{
+//     button.forEach((item)=>{
+//         if (item.innerHTML=="Click"){
+//             item.innerHTML="Clicked"
+//         }
+//         else{
+//             item.innerHTML="Click"
+//         }
+//     })
+// }
+
+// let btn =document.getElementById("btn")
+
+// btn.addEventListener("click",()=>{
+//     btn.innerHTML = btn.innerHTML=="Click"?"Clicked":"Click"
+// })
+
+// let user = document.getElementById("userName")
+// user.addEventListener("change",(e)=>{
+//     console.log(e.target.value);
+    
+// })
+
+
+let user = document.getElementById("userName")
+
+const usershow = ()=>{
+    console.log(user.value);    
+}
